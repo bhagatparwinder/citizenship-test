78ee551aa055710f1be9fb12f22aefe352847fda
Parwinder Bhagat <singhparwinder@me.com>
2018-01-17 19:44:05 -0600

    added 4 comp for the app
    - app-header
    - question

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NgModule } from '@angular/core';
+import { QuestionComponent } from './question/question.component';
+import { ScoreComponent } from './score/score.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    QuestionComponent,
+    FeedbackComponent,
+    ScoreComponent
   ],
   imports: [
     BrowserModule
