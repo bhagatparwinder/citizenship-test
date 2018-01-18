e788e0f4d04ff7d5d0ad249182d69a92eb034527
Parwinder Bhagat <singhparwinder@me.com>
2018-01-17 21:49:49 -0600

    updated question comp, hardcoded values

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -6,6 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question.component.css']
 })
 export class QuestionComponent implements OnInit {
+  data: any = {
+      order: 1,
+      desc: 'Name one U.S. territory?',
+      choices: [{
+              id: 'A',
+              desc: 'Bermuda'
+          },
+          {
+              id: 'B',
+              desc: 'Guam'
+          },
+          {
+              id: 'C',
+              desc: 'Cayman Islands'
+          },
+          {
+              id: 'D',
+              desc: 'Haiti'
+          }
+      ]
+  };
 
   constructor() { }
 
