fa3b577ae00106363f31169fabfc531f523bb59e
Ashutosh Devkota <ashutosh.devkota@gmail.com>
2019-02-27 22:35:17 -0600

    feat: introduce animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { NgModule } from '@angular/core';
 import { QuestionComponent } from './question/question.component';
@@ -18,7 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    BrowserAnimationsModule,
+    HttpClientModule,
   ],
   providers: [
     DataService
--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,17 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { DataService } from '../shared/data-service/data.service';
+import { state, style, trigger, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.css']
+  styleUrls: ['./question.component.css'],
+  animations: [
+    trigger('changeQuestionState', [
+      transition('start-next => finish', [
+        animate('0.6s', keyframes([
+          style({transform: 'translateX(0)', offset: 0}),
+          style({transform: 'translateX(100%)', opacity: 0.1, offset: 0.49}),
+          style({opacity: 0, offset: 0.5}),
+          style({transform: 'translateX(-100%)', offset: 0.51}),
+          style({opacity: 0.1, offset: 0.52}),
+          style({transform: 'translateX(0)', opacity: 1, offset: 1.0})
+        ]))
+      ]),
+      transition('start-prev => finish', [
+        animate('0.6s', keyframes([
+          style({transform: 'translateX(0)', offset: 0}),
+          style({transform: 'translateX(-100%)',  opacity: 0.1, offset: 0.49}),
+          style({opacity: 0, offset: 0.5}),
+          style({transform: 'translateX(100%)', offset: 0.51}),
+          style({opacity: 0.1, offset: 0.52}),
+          style({transform: 'translateX(0)', opacity: 1, offset: 1.0})
+        ]))
+      ]),
+
+    ])
+  ]
 })
 export class QuestionComponent implements OnInit {
   public question: any;
   public isLast: boolean;
   private selectedChoice;
+  public _currentQuestionChangeState = 'start';
 
-  constructor(private dataService: DataService) {
+  constructor(private dataService: DataService,
+    private cd: ChangeDetectorRef) {
     this.question = this.initQuestions();
     try {
       this.dataService.getQuestionSubject().subscribe((data) => this.handleQuestions(data));
@@ -27,13 +55,23 @@ export class QuestionComponent implements OnInit {
     this.selectedChoice = null;
     this.question = data.question || {};
     this.isLast = data.isLast;
+    this.currentQuestionChangeState = 'finish';
   }
 
+  set currentQuestionChangeState(newState) {
+    this._currentQuestionChangeState = newState;
+    this.cd.detectChanges();
+  }
+  get currentQuestionChangeState() {
+    return this._currentQuestionChangeState;
+  }
   nextQuestion(): void {
+    this.currentQuestionChangeState = 'start-next';
     this.dataService.sendQuestion({next: true});
   }
 
   prevQuestion(): void {
+    this.currentQuestionChangeState = 'start-prev';
     this.dataService.sendQuestion({next: false});
   }
 
